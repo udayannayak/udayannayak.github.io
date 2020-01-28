75206f33d9cfb337b9991207f9ee4eb4fc216bfb
Udayan Nayak <unayak@affirma.com>
2020-01-28 23:16:24 +0530

    Updates

---
+++ precache-manifest.a301c4640afb58bf035370e16903ca71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "404890ac7bdb3fcbabac8b04cc3ab49b",
+    "url": "/star-wars/index.html"
+  },
+  {
+    "revision": "bf378d188e260436e36b",
+    "url": "/star-wars/static/css/main.191c9796.chunk.css"
+  },
+  {
+    "revision": "e2a6c58e3d996aac35d9",
+    "url": "/star-wars/static/js/2.2d85fa2c.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/star-wars/static/js/2.2d85fa2c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bf378d188e260436e36b",
+    "url": "/star-wars/static/js/main.c1dd6cc5.chunk.js"
+  },
+  {
+    "revision": "bb19ec81ea61bb80c6e1",
+    "url": "/star-wars/static/js/runtime-main.73f5171a.js"
+  }
+]);
