c904e6c8e91fa119980fc7f82b85aaa92a1ae697
udayannayak <47316468+udayannayak@users.noreply.github.com>
2020-01-28 22:47:03 +0530

    Add files via upload

---
+++ precache-manifest.6ff97237a6fe7320bcdcbbcb8be2ada0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc8ddcdd8527d5bf79a2d9429a896047",
+    "url": "/index.html"
+  },
+  {
+    "revision": "672eb0cd2c57dba10e69",
+    "url": "/static/css/main.191c9796.chunk.css"
+  },
+  {
+    "revision": "e2a6c58e3d996aac35d9",
+    "url": "/static/js/2.2d85fa2c.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.2d85fa2c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "672eb0cd2c57dba10e69",
+    "url": "/static/js/main.4a4cab5e.chunk.js"
+  },
+  {
+    "revision": "39ebd2ce3a725c6e12de",
+    "url": "/static/js/runtime-main.76b91643.js"
+  }
+]);
